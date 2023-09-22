cafce0bd45a1f8c3b1bf0138adafbeee34795762
JimB1 <112900194+jb71763n@users.noreply.github.com>
2023-09-22 16:18:13 -0400

    11 simple s3 bucket (#12)
    * #11 install terraform provider, create S3 bucket
    
    * #11 Added project findings for the random S3 naming to readme.md

--- main.tf
+++ main.tf
@@ -6,15 +6,29 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
   }
 }
 
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs
 provider "random" {
   # Configuration options
 }
 resource "random_string" "bucket_name" {
-  length           = 16
+  length           = 32
   special          = false
+  lower            = true
+  upper            = false
+}
+
+ provider "aws" {
+  # Configuration options
+}
+ resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
 }
 
 output "random_bucket_name" {
