0f69472bcf56ce4fd6f2f62fdb5ecc9485789020
JimB1 <112900194+jb71763n@users.noreply.github.com>
2023-09-29 12:22:39 -0400

    #27 configured static website hosting, uploaded index and error html (#28)

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,7 @@ output "bucket_name" {
     description = "bucket name for our static website hosting"
     value = module.terrahouse_aws.bucket_name
 }
+output "website_endpoint" {
+    description = "bucket endpoint for static website hosting"
+    value = module.terrahouse_aws.website_endpoint
+}
