15b515db78cb1c36a91186b9694f2f0fdcbe998a
James Burke <jb71763n@yahoo.com>
2023-09-29 21:59:30 +0000

    #29 corrected html files

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -23,6 +23,7 @@ resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "index.html"
   source = var.index_html_filepath
+  content_type = "text/html"
   etag = filemd5(var.index_html_filepath)
 }
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
@@ -30,6 +31,7 @@ resource "aws_s3_object" "error_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "error.html"
   source = var.error_html_filepath
+  content_type = "text/html"
   etag = filemd5(var.error_html_filepath)
 }
 
