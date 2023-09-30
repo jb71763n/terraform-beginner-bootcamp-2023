825f6739c853b659b4cf16d76522c2901d221ce9
JimB1 <112900194+jb71763n@users.noreply.github.com>
2023-09-30 11:11:03 -0400

    #32 only upload .html files if content version has changed (#32)

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -25,6 +25,10 @@ resource "aws_s3_object" "index_html" {
   source = var.index_html_filepath
   content_type = "text/html"
   etag = filemd5(var.index_html_filepath)
+  lifecycle {
+    replace_triggered_by = [ terraform_data.content_version.output ]
+    ignore_changes = [etag]
+  }
 }
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_object
 resource "aws_s3_object" "error_html" {
@@ -33,6 +37,10 @@ resource "aws_s3_object" "error_html" {
   source = var.error_html_filepath
   content_type = "text/html"
   etag = filemd5(var.error_html_filepath)
+   lifecycle {
+    replace_triggered_by = [ terraform_data.content_version.output ]
+    ignore_changes = [etag]
+  }
 }
 
 resource "aws_s3_bucket_policy" "bucket_policy" {
@@ -56,4 +64,8 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
       }
     }
   })
+}
+
+resource "terraform_data" "content_version" {
+  input = var.content_version
 }
