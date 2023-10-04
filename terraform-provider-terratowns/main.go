06a4aae72c526062ca7cf3ccc2c7a206bbc11065
James Burke <jb71763n@yahoo.com>
2023-10-04 22:18:21 +0000

    #45 setup go files for terratown provider

---
+++ terraform-provider-terratowns/main.go
@@ -0,0 +1,10 @@
+package main
+// package main: Declares the package name
+// The main package is special in Go, it's where the execution of the program starts
+
+// import "fmt": Imports the fmt package, which contains functions for formatted I/O.
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+}
