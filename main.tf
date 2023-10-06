6c6767cd409b5912da70d3036d4a588837a51b94
James Burke <jb71763n@yahoo.com>
2023-10-06 20:48:49 +0000

    #53 first test to terratowns competed

--- main.tf
+++ main.tf
@@ -21,22 +21,20 @@ terraform {
   #}
 
 }
-
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = "https://terratowns.cloud/api"
+  user_uuid = "ea0e39b4-1a37-4967-8053-122a620913ef"
+  token = "c21ed203-80bc-4e9d-aca9-eb006b88a5e1"
 }
-
-module "terrahouse_aws" {
-  source = "./modules/terrahouse_aws"
-  user_uuid = var.user_uuid
-  bucket_name = var.bucket_name
-  error_html_filepath = var.error_html_filepath
-  index_html_filepath = var.index_html_filepath
-  content_version = var.content_version
-  assets_path = var.assets_path
-}
+#module "terrahouse_aws" {
+#  source = "./modules/terrahouse_aws"
+#  user_uuid = var.user_uuid
+  #bucket_name = var.bucket_name
+ # error_html_filepath = var.error_html_filepath
+ # index_html_filepath = var.index_html_filepath
+ # content_version = var.content_version
+ # assets_path = var.assets_path
+#}
 
 resource "terratowns_home" "home" {
   name = "Coffee for All 2023!"
--- modules/terrahouse_aws/variables.tf
+++ modules/terrahouse_aws/variables.tf
@@ -1,23 +1,25 @@
 variable "user_uuid" {
-  description = "User UUID"
-  
-  type = string
-
+  description = "The UUID of the user"
+  type        = string
   validation {
-    condition     = can(regex("^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", var.user_uuid))
-    error_message = "user_uuid must be in the format of a UUID (e.g., 123e4567-e89b-12d3-a456-426655440000)"
+    condition        = can(regex("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$", var.user_uuid))
+    error_message    = "The user_uuid value is not a valid UUID."
   }
 }
-variable "bucket_name" {
-  description = "AWS S3 Bucket Name"
-  
-  type = string
 
-  validation {
-    condition     = can(regex("^[a-zA-Z0-9.-]{3,63}$", var.bucket_name))
-    error_message = "bucket_name must be a valid AWS S3 bucket name (3-63 characters, letters, numbers, hyphens, and periods allowed)"
-  }
-}
+#variable "bucket_name" {
+#  description = "The name of the S3 bucket"
+#  type        = string
+#
+#  validation {
+#    condition     = (
+#      length(var.bucket_name) >= 3 && length(var.bucket_name) <= 63 && 
+#      can(regex("^[a-z0-9][a-z0-9-.]*[a-z0-9]$", var.bucket_name))
+#    )
+#    error_message = "The bucket name must be between 3 and 63 characters, start and end with a lowercase letter or number, and can contain only lowercase letters, numbers, hyphens, and dots."
+#  }
+#}
+
 variable "index_html_filepath" {
   description = "The file path for index.html"
   type        = string
@@ -37,19 +39,18 @@ variable "error_html_filepath" {
     error_message = "The provided path for error.html does not exist."
   }
 }
-variable "assets_path" {
-  description = "The file path for assets"
-  type        = string
-}
+
 variable "content_version" {
-  description = "Content Version"
-  
-  type = number
+  description = "The content version. Should be a positive integer starting at 1."
+  type        = number
 
   validation {
     condition     = var.content_version > 0 && floor(var.content_version) == var.content_version
-    error_message = "content_version must be a positive integer starting at 1"
+    error_message = "The content_version must be a positive integer starting at 1."
   }
+}
 
-  default = 1 # You can set a default value if needed
+variable "assets_path" {
+  description = "Path to assets folder"
+  type = string
 }
